7046c8a01a3bb0299c788c0da52e5f9f355a1529
vinhtran <tranhoangvinh.313@gmail.com>
2022-04-26 21:21:09 +0700

    first commit

---
+++ components/layouts/header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <p>This is header</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>This is layout</h1>
+    <LayoutsHeader></LayoutsHeader>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
